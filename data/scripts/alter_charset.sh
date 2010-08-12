2c3ffb56d120c3c9f46415144fb43b7e1d6dbbbe
Mark Veltzer <mark.veltzer@gmail.com>
2010-08-12 15:02:13 +0300

    more improvements to the script

--- data/scripts/alter_charset.sh
+++ data/scripts/alter_charset.sh
@@ -9,7 +9,7 @@ COLLATION='utf8_unicode_ci'
 
 # here we go...
 QUERY="SELECT table_name FROM information_schema.TABLES WHERE table_schema = '$DB';"
-TABLES=$(mysql -u $USER --password=$PASS $DB --execute="$QUERY" | sed 's/|//g' | tail -n +2)
+TABLES=$(mysql -u $USER --password=$PASS $DB --batch --skip-column-names --execute="$QUERY")
 for TABLE in $TABLES; do
 	echo "ALTER TABLE $TABLE ......"
 	mysql -u $USER --password=$PASS $DB -e "ALTER TABLE $TABLE CONVERT TO CHARSET $CHARSET"
