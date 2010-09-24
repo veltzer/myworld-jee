47316f2138a45f30032a3ac2a4f30ebf25ce17e4
Mark Veltzer <mark.veltzer@gmail.com>
2010-09-24 13:57:02 +0200

    better backup script

--- data/scripts/backup.sh
+++ data/scripts/backup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-#x=`date +%y%m%d%H%M%S`
-DIR=~/Dropbox/db
+x=`date +%y%m%d%H%M%S`
+DIR=~/Dropbox/db/$x
+mkdir $DIR
 OPTS="--complete-insert --skip-dump-date"
 mysqldump $OPTS myworld > $DIR/data_and_schema.sql
 mysqldump $OPTS myworld --no-create-info > $DIR/data_only.sql
 mysqldump $OPTS myworld --no-data > $DIR/schema_only.sql
-./scripts/stats.sh > $DIR/stats.txt
+./stats.sh > $DIR/stats.txt
