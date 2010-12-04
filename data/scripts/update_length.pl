aab97715ec10d20dbb6debd5a170f3195411d002
Mark Veltzer <mark.veltzer@gmail.com>
2010-12-04 14:40:41 +0200

    added support for video

--- data/scripts/update_length.pl
+++ data/scripts/update_length.pl
@@ -3,6 +3,7 @@
 use strict;
 use diagnostics;
 use MP3::Info qw();
+use Video::Info qw();
 use File::Glob ':glob';
 use DBI;
 
@@ -33,27 +34,39 @@ my($do_all)=0;
 my($sql);
 #$sql='select * from TbWkWork';
 # selecting only works which are audio (selecting all as above will NOT work...)
-$sql='select * from TbWkWork where typeId in (1,2,3,5,12,14)';
+$sql='select TbWkWork.id,TbWkWork.name,TbWkWork.typeId from TbWkWork,TbWkWorkType where TbWkWork.typeId=TbWkWorkType.id and TbWkWorkType.name in (\'audio book\',\'audio course\',\'audio show\',\'video course\')';
 if($do_all) {
 } else {
-	$sql.=" and ( length is NULL or size is NULL or chapters is NULL )";
+	$sql.=' and ( TbWkWork.length is NULL or TbWkWork.size is NULL or TbWkWork.chapters is NULL )';
 }
 if($debug) {
-	print "sql is [".$sql."]\n";
+	print 'sql is ['.$sql.']'."\n";
 }
 my($counter)=0;
 my($sth)=$dbh->prepare($sql);
-$sth->execute() or die "SQL Error: $DBI::errstr\n";
+$sth->execute() or die 'SQL Error: ['.$DBI::errstr.']'."\n";
 my($rowhashref);
 while($rowhashref=$sth->fetchrow_hashref()) {
-	my($row_id)=$rowhashref->{'id'};
-	my($name)=$rowhashref->{'name'};
-	if($prog) {
-		print "doing $name\n";
+	my($f_id)=$rowhashref->{'id'};
+	my($f_name)=$rowhashref->{'name'};
+	my($f_typeId)=$rowhashref->{'typeId'};
+	my($type);
+	if($f_typeId==2 || $f_typeId==3 || $f_typeId==5) {
+		$type='audio';
+	} else {
+		$type='video';
 	}
-	my($folder)="/home/mark/links/topics_archive/audio/abooks/by_title_name/$name";
+	if($prog) {
+		print 'doing ['.$f_name.']'."\n";
+	}
+	my($folder);
+	if($type eq 'audio') {
+		$folder='/home/mark/links/topics_archive/audio/abooks/by_title_name/'.$f_name;
+	} else {
+		$folder='/home/mark/links/topics_archive/video/emovies/by_title_name/'.$f_name;
+	}
 	if(! -d $folder) {
-		die("'$folder' is not a folder!");
+		die('['.$folder.'] is not a folder');
 	}
 	# now calculate the length
 	my(@file_list)=<$folder/*>;
@@ -64,32 +77,42 @@ while($rowhashref=$sth->fetchrow_hashref()) {
 		if($debug) {
 			print $filename."\n";
 		}
-		my($res)=MP3::Info::get_mp3info($filename);
-		if($debug) {
-			while(my($key,$value)=each(%$res)) {
-				print "$key -> $value\n";
+		if($type eq 'audio') {
+			my($res)=MP3::Info::get_mp3info($filename);
+			if($debug) {
+				while(my($key,$value)=each(%$res)) {
+					print $key.' -> '.$value."\n";
+				}
 			}
+			if(!exists($res->{'SECS'})) {
+				die('have not found SECS for ['.$filename.']');
+			}
+			if(!exists($res->{'SIZE'})) {
+				die('have not found SIZE for ['.$filename.']');
+			}
+			$stat_secs+=$res->{'SECS'};
+			$stat_size+=$res->{'SIZE'};
 		}
-		if(exists($res->{"SECS"})) {
-			$stat_secs+=$res->{"SECS"};
-		} else {
-			die("have not found SECS for $filename");
+		if($type eq 'video') {
+			my($info)=Video::Info->new(-file=>$filename);
+			my($curr_secs)=$info->duration();
+			my($curr_size)=$info->filesize();
+			if($debug) {
+				print 'curr secs is ['.$curr_secs.']'."\n";
+				print 'curr size is ['.$curr_size.']'."\n";
+			}
+			$stat_secs+=$curr_secs;
+			$stat_size+=$curr_size;
 		}
-		if(exists($res->{"SIZE"})) {
-			$stat_size+=$res->{"SIZE"};
-		} else {
-			die("have not found SIZE for $filename");
-		}
-		$res=undef;
 	}
 	my($chapters)=scalar(@file_list);
 	if($prog) {
 		print 'found size='.$stat_size.', secs='.$stat_secs.',chapters='.$chapters."\n";
 	}
 	# now update the database...
-	$dbh->do("update TbWkWork set length=? where id=?",undef,$stat_secs,$row_id);
-	$dbh->do("update TbWkWork set size=? where id=?",undef,$stat_size,$row_id);
-	$dbh->do("update TbWkWork set chapters=? where id=?",undef,scalar(@file_list),$row_id);
+	$dbh->do('update TbWkWork set length=? where id=?',undef,$stat_secs,$f_id);
+	$dbh->do('update TbWkWork set size=? where id=?',undef,$stat_size,$f_id);
+	$dbh->do('update TbWkWork set chapters=? where id=?',undef,scalar(@file_list),$f_id);
 	$counter++;
 }
 $dbh->commit();
