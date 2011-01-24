3e188627bfb52341e17284886d1a9cee62f52575
Mark Veltzer <mark.veltzer@gmail.com>
2011-01-24 03:53:16 +0200

    removed the data and moved it into dbdata

--- data/scripts/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-x=`date +%y%m%d%H%M%S`
-DIR=~/Dropbox/db/$x
-mkdir $DIR
-OPTS="--complete-insert --skip-dump-date"
-mysqldump $OPTS myworld > $DIR/data_and_schema.sql
-mysqldump $OPTS myworld --no-create-info > $DIR/data_only.sql
-mysqldump $OPTS myworld --no-data > $DIR/schema_only.sql
-./scripts/stats.sh > $DIR/stats.txt
--- data/scripts/stats.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for x in `echo "show tables" | mysql --skip-column-names`; do
-	echo $x `echo "select count(*) from $x" | mysql --skip-column-names`
-done
