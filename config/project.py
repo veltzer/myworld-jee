2d24beff246e461207f3be7bf43b0f508ccc4c57
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:02 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "myworld-jee"
-project_long_description = "My world system in JEE"
-project_year_started = 2010
-project_description = """My world system in JEE"""
+description_short = "My world system in JEE"
